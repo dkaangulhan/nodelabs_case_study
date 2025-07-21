07ed9bfa5949f6c901da5025655db5807fc4691f
dkaangulhan <dkaangulhan@gmail.com>
2025-07-21 17:37:44 +0300

    generated files excluded from analyzer

--- packages/nodelabs_ui/lib/src/widgets/nodelabs_error_box.dart
+++ packages/nodelabs_ui/lib/src/widgets/nodelabs_error_box.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// This is error box that displays errors inside.
--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -6,9 +6,11 @@ import 'package:nodelabs_case_study/counter/counter.dart';
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(App(
-        authRepository: AuthRepository(ApiClient()),
-      ));
+      await tester.pumpWidget(
+        App(
+          authRepository: AuthRepository(ApiClient()),
+        ),
+      );
       expect(find.byType(CounterPage), findsOneWidget);
     });
   });
