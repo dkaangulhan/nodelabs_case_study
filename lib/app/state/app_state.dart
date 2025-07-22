93b652e73e3b9a9ef3e944ade36af02ae9f91e41
dkaangulhan <dkaangulhan@gmail.com>
2025-07-22 09:41:07 +0300

    app state name refactored

--- lib/app/state/app_state.dart
+++ lib/app/state/app_state.dart
@@ -1,19 +1,19 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 
-final class App extends Equatable {
-  const App({required this.isLoggedIn});
+final class AppState extends Equatable {
+  const AppState({required this.isLoggedIn});
 
   final bool isLoggedIn;
 
   @override
   List<Object> get props => [isLoggedIn];
 
-  App copyWith({
+  AppState copyWith({
     bool? isLoggedIn,
   }) {
-    return App(isLoggedIn: isLoggedIn ?? this.isLoggedIn);
+    return AppState(isLoggedIn: isLoggedIn ?? this.isLoggedIn);
   }
 }
 
-final appState = ValueNotifier(const App(isLoggedIn: false));
+final appState = ValueNotifier(const AppState(isLoggedIn: false));
