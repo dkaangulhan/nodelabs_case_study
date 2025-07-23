9e32f1411c757889c19b23b744ea0f630abd3045
dkaangulhan <dkaangulhan@gmail.com>
2025-07-23 14:12:15 +0300

    limited offer blur upadte

--- lib/limited_offer/view/limited_offer.dart
+++ lib/limited_offer/view/limited_offer.dart
@@ -9,7 +9,7 @@ import 'package:nodelabs_ui/nodelabs_ui.dart';
 part '_package.dart';
 part '_perk.dart';
 
-const _blur = 10.0;
+final _blur = ImageFilter.blur(sigmaX: 10, sigmaY: 10);
 const _bottomSheetBorderRadius = Radius.circular(
   32,
 );
@@ -31,13 +31,10 @@ class LimitedOffer extends StatelessWidget {
         backgroundColor: Colors.transparent,
         body: AnimatedContainer(
           duration: const Duration(milliseconds: 300),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: _blur, sigmaY: _blur),
-            child: const SafeArea(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: _BottomSheet(),
-              ),
+          child: const SafeArea(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: _BottomSheet(),
             ),
           ),
         ),
@@ -51,57 +48,109 @@ class _BottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: _bottomSheetPadding,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
-        borderRadius: const BorderRadius.only(
-          topLeft: _bottomSheetBorderRadius,
-          topRight: _bottomSheetBorderRadius,
+    return BackdropFilter(
+      filter: _blur,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          borderRadius: const BorderRadius.only(
+            topLeft: _bottomSheetBorderRadius,
+            topRight: _bottomSheetBorderRadius,
+          ),
         ),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            context.t.limitedOffer,
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  fontSize: _titleFontSize,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Positioned.fill(
+              child: ImageFiltered(
+                imageFilter: ImageFilter.blur(sigmaX: 250, sigmaY: 250),
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Positioned(
+                      top: -60,
+                      left: -20,
+                      right: 0,
+                      child: Center(
+                        child: Container(
+                          width: 200,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            backgroundBlendMode: BlendMode.plus,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: -100,
+                      left: 0,
+                      right: 0,
+                      child: Center(
+                        child: Container(
+                          width: 200,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            backgroundBlendMode: BlendMode.plus,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-          ),
-          Text(
-            context.t.limitedOfferDescription,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          const _Perks(),
-          const SizedBox(
-            height: 16,
-          ),
-          Text(
-            context.t.pickPackageToUnlock,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const _PackageList(),
-          const SizedBox(
-            height: 16,
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: NodelabsPrimaryButton(
-              onTap: () {},
-              title: context.t.seeAllTokens,
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: _bottomSheetPadding,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    context.t.limitedOffer,
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          fontSize: _titleFontSize,
+                        ),
+                  ),
+                  Text(
+                    context.t.limitedOfferDescription,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const _Perks(),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    context.t.pickPackageToUnlock,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const _PackageList(),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: NodelabsPrimaryButton(
+                      onTap: () {},
+                      title: context.t.seeAllTokens,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
